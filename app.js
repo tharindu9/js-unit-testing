703def82eebea67ed98a2106e2d38b7ce5e2b403
tharindu <tharindu@dreamsquadgroup.com>
2020-06-06 20:35:05 +0530

    add sinon spy test case

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-module.exports = {
-
-    sayHello: () => {
+class Cal {
+    sayHello(){
         return 'hello';
-    },
+    }
 
-    addNum: (num1 , num2) => {
+    addNum(num1 , num2) {
         return num1 + num2;
-    },
+    }
+
+    subNum(num1 , num2){
+        if(num1 > num2){
+            return num1 - num2;
+        }
+        else{
+            return num2 - num1;
+        }
+    }
+
+    mulNum(num1, num2){
+        return num1*num2;
+    }
+
+    devNum(num1,num2){
+        return num1/num2;
+    }
+
+    calOparations(num1 , num2 , oparation){
+        if (oparation === 'add') {
+            this.addNum(num1 , num2)
+        }
+        else if (oparation === 'sub'){
+            subNum(num1,num2)
+        }
+        else if (oparation === 'mul'){
+            mulNum(num1,num2)
+        }
+        else {
+            devNum(num1 , num2)
+        }
+    }
+
 }
-
-
+module.exports = Cal;
 
 
 
