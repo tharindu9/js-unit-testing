90a31df5efb3a515fdd8ce16e079e5e42f5303b5
tharindu <tharinduariyarathna94@gmail.com>
2020-07-01 20:50:30 +0530

    Add chai-http for api testing and write simple api test case

--- app/app.js
+++ app/app.js
@@ -1,100 +1,28 @@
-var circle =require('./circle_object')
+const express = require("express");
 
+const app = express();
 
-var Car = function(name, model, year) {
-    this.name = name;
-    this.model = model;
-    this.year = year;
-  }
+app.use(express.json());
 
-  Car.prototype.getName = function(){
-      return this.name;
-  }
-  Car.prototype.getModel = function(){
-    return this.model;
-}
-var myCar = new Car("suzuki","shift",2020)
-var carName = myCar.getName();
-console.log(carName)
-console.log(myCar)
+app.get("/", (req, res) => {
+    res.json({ status: "success", message: "Welcome To Testing API" });
+  });
+  
+  app.post("/add", (req, res) => {
+    const { num1, num2 } = req.body;
+    const add = (num1, num2) => {
+      return num1 + num2;
+    };
+    res.json({
+      status: "success",
+      message: "Welcome To Testing API",
+      result: add(num1, num2)
+    });
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
-var HonadaCar = function(){
-   this.brand = "Honda";
-}
-
-HonadaCar.prototype = new Car("new car", "new model", 2020);
-HonadaCar.prototype.speed = function(){
-    return this.model;
-}
-var hc = new HonadaCar();
-var hondaSpeed = hc.getModel();
-console.log("< honda speed :::: ",hondaSpeed)
-
-class Cal {
-    sayHello(){
-        return 'hello';
-    }
-
-    addNum(num1 , num2) {
-        return num1 + num2;
-    }
-
-    subNum(num1 , num2){
-        if(num1 > num2){
-            return num1 - num2;
-        }
-        else{
-            return num2 - num1;
-        }
-    }
-
-    mulNum(num1, num2){
-        return num1*num2;
-    }
-
-    devNum(num1,num2){
-        return num1/num2;
-    }
-
-    calOparations(num1 , num2 , oparation){
-        if (oparation === 'add') {
-            this.addNum(num1 , num2)
-        }
-        else if (oparation === 'sub'){
-            subNum(num1,num2)
-        }
-        else if (oparation === 'mul'){
-            mulNum(num1,num2)
-        }
-        else {
-            devNum(num1 , num2)
-        }
-    }
-    x = 0;
-    func1(num){
-        var innerVar = 10;
-        var that = this
-        var innerFunction = function(){
-        }
-        innerFunction()
-    }
-    
-}
-
-var claObj = new Cal()
-
-var func1 = function(num){
-    var innerVar = 10;
-    var innerFunction = function(){
-    }
-    innerFunction()
-}
-func1(5);
-
-var arr = [3,5]
-
-var add = function(arg1, arg2){
-    return arg1+arg2;
-}
-module.exports = Cal;
+module.exports = app;
 
--- app/test/appTest.js
+++ app/test/appTest.js
@@ -1,15 +1,46 @@
 
 var app         = require('../app')
-var expect      = require('chai').expect
+//var expect      = require('chai').expect
 var assert      = require('chai').assert
 const jsdom     = require("jsdom");
-const sinon     = require('sinon')
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-const { window } = new JSDOM();
+const sinon     = require('sinon');
+const chai      = require("chai");
+const chaiHttp  = require("chai-http");
+const { expect } = chai;
 
-var $ = jQuery = require('jquery')(window);
-var calObj = new app();
+// const { JSDOM } = jsdom;
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// const { window } = new JSDOM();
+// var $ = jQuery = require('jquery')(window);
+
+chai.use(chaiHttp);
+
+describe("Server!", () => {
+    it("welcomes user to the api", done => {
+      chai
+        .request(app)
+        .get("/")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.status).to.equals("success");
+          expect(res.body.message).to.equals("Welcome To Testing API");
+          done();
+        });
+    });
+    it("adds 2 numbers", done => {
+        chai
+          .request(app)
+          .post("/add")
+          .send({ num1: 5, num2: 5 })
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res.body.status).to.equals("success");
+            expect(res.body.result).to.equals(10);
+            done();
+          });
+      });
+});
+/*
 describe('App ' , ()=>{
     it('app sould return hello',()=>{
         let result = calObj.sayHello(); 
@@ -44,4 +75,4 @@ describe('App ' , ()=>{
     })
     
 })
-
+*/
